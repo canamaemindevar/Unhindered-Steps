76575f2436992319fb5f753cb9c4f78bc4e7d532
canamaemindevar <94639136+canamaemindevar@users.noreply.github.com>
2023-10-05 11:04:19 +0300

    turkish and english version done

--- Unhindered Steps/String+Extensions.swift
+++ Unhindered Steps/String+Extensions.swift
@@ -11,10 +11,6 @@ extension String {
         return NSLocalizedString(self, comment: "\(self)_comment")
     }
 
-    func localized(_ args: [CVarArg]) -> String {
-        return localized(args)
-    }
-
     func localized(_ args: CVarArg...) -> String {
         return String(format: localized, args)
     }
